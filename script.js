1080f9f97d2cbf66a32f1573c84d0ecdafca4d1e
Niharika2016 <145590583+Niharika2016@users.noreply.github.com>
2024-09-04 02:45:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1cf6b6ef6fmsh42ac10a7f6a641cp13b522jsn2f6d5a2b2b83',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
